ef40da510d22411c4f7a41457a3b4c80bacc1321
Aravind Lal <aravindlal2006@gmail.com>
2026-04-29 22:32:34 +0530

    refactor: replace PlayerScreen and persistent mini-player with a unified PlayerSheet component

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar, StyleSheet } from 'react-native';
 import { NavigationContainer, DarkTheme } from '@react-navigation/native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { PlaybackProvider } from './context/PlaybackContext';
 import { RootNavigator } from './navigation/RootNavigator';
@@ -10,20 +10,22 @@ import { colors } from './theme';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <PlaybackProvider>
-        <NavigationContainer theme={DarkTheme}>
-          <StatusBar
-            barStyle="light-content"
-            backgroundColor={colors.background}
-            translucent={false}
-          />
-          <SafeAreaView edges={['top', 'left', 'right']} style={styles.safeArea}>
-            <RootNavigator />
-          </SafeAreaView>
-        </NavigationContainer>
-      </PlaybackProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <PlaybackProvider>
+          <NavigationContainer theme={DarkTheme}>
+            <StatusBar
+              barStyle="light-content"
+              backgroundColor={colors.background}
+              translucent={false}
+            />
+            <SafeAreaView edges={['top', 'left', 'right']} style={styles.safeArea}>
+              <RootNavigator />
+            </SafeAreaView>
+          </NavigationContainer>
+        </PlaybackProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
