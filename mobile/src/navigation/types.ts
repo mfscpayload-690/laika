301b481ead7143556d3f0d96d992fe20173a47bf
Aravind Lal <aravindlal2006@gmail.com>
2026-04-29 16:34:51 +0530

    feat(types): add type definitions for navigation parameters

---
+++ mobile/src/navigation/types.ts
@@ -0,0 +1,10 @@
+export type RootStackParamList = {
+  MainTabs: undefined;
+  Player: undefined;
+};
+
+export type MainTabsParamList = {
+  Home: undefined;
+  Search: undefined;
+  Library: undefined;
+};
