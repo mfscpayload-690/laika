874016e579166bc74912c7f5ea50cbc411ff27a5
Aravind Lal <aravindlal2006@gmail.com>
2026-04-30 22:57:52 +0530

    feat: add user profile integration to HomeScreen and configure Kotlin versioning in Android build

--- mobile/src/types/modules.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@react-native-async-storage/async-storage' {
-  import { AsyncStorageStatic } from '@react-native-async-storage/async-storage/lib/typescript/AsyncStorage';
-  const AsyncStorage: AsyncStorageStatic;
-  export default AsyncStorage;
-}
